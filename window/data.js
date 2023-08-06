8b02366c2d215279718650ab809a3e417d8cb57c
aavasare47 <aniketavasare2004@gmail.com>
2023-08-06 12:11:03 +0530

    project 2

--- window/data.js
+++ window/data.js
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         name: "Monkey D. Luffy",
-        img: "./Monkey_D._Luffy_Portrait (1).jpg",
+        img: "./images/Monkey_D._Luffy_Portrait (1).jpg",
         title: "Captain",
         bounty: "3,000,000,000",
         description: "Monkey D. Luffy, also known as \"Straw Hat Luffy\" is the founder and captain of the increasingly infamous and powerful Straw Hat Pirates,as well as the most powerful of its top fighters. He desires to find the legendary treasure left behind by the late Gol D. Roger and thereby become the Pirate King, which would help facilitate an unknown dream of his that he has told only to Shanks, his brothers, and crew. He believes that being the Pirate King means having the most freedom in the world.",
@@ -10,9 +10,73 @@ export default [
     {
         id:2,
         name: "Roronoa Zoro",
-        img: "./Roronoa_Zoro_Portrait (1).jpg",
+        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
         title: "Swordsman/First Mate",
         bounty: "1,111,000,000",
-        description: "Roronoa Zoro,[20] also known as \"Pirate Hunter\" Zoro,[9] is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet,[4] and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy.[21][22] Formerly a bounty hunter,[6] he is the second member of Luffy's crew and the first to join it"
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+    },
+    {
+        id:3,
+        name: "Nami",
+        img: "./images/Nami_Portrait.jpg",
+        title: "Navigator",
+        bounty: "366,000,000",
+        description: "\"Cat Burglar\" Nami is the navigator of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. She is the third member of the crew and the second to join, doing so during the Orange Town Arc. She is the adoptive sister of Nojiko after the two were orphaned and taken in by Bell-mère."
+    },
+    {
+        id:4,
+        name: "Usopp",
+        img: "./images/Usopp_Portrait.jpg",
+        title: "Sniper",
+        bounty: "500,000,000",
+        description: "\"God\" Usopp is the sniper of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the fourth member of the crew and the third to join, doing so at the end of the Syrup Village Arc. Although he left the crew during the Water 7 Arc, he rejoined at the end of the Post-Enies Lobby Arc."
+    },
+    {
+        id:5,
+        name: "Vinsmoke Sanji",
+        img:"./images/Sanji_Portrait.jpg",
+        title: "Cook",
+        bounty: "1,032,000,000",
+        description: "\"Black Leg\" Sanji, born as Vinsmoke Sanji, is the cook of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the fifth member of the crew and the fourth to join, doing so at the end of the Baratie Arc."
+    },
+    {
+        id:2,
+        name: "Roronoa Zoro",
+        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
+        title: "Swordsman/First Mate",
+        bounty: "1,111,000,000",
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+    },
+    {
+        id:2,
+        name: "Roronoa Zoro",
+        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
+        title: "Swordsman/First Mate",
+        bounty: "1,111,000,000",
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+    },
+    {
+        id:2,
+        name: "Roronoa Zoro",
+        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
+        title: "Swordsman/First Mate",
+        bounty: "1,111,000,000",
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+    },
+    {
+        id:2,
+        name: "Roronoa Zoro",
+        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
+        title: "Swordsman/First Mate",
+        bounty: "1,111,000,000",
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+    },
+    {
+        id:2,
+        name: "Roronoa Zoro",
+        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
+        title: "Swordsman/First Mate",
+        bounty: "1,111,000,000",
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
     }
 ]
