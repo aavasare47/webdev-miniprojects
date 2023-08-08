bae09ea9614b51a15a8f415bc8f78852c96f0e7a
aavasare47 <aniketavasare2004@gmail.com>
2023-08-08 21:54:31 +0530

    project 2 final commit

--- window/data.js
+++ window/data.js
@@ -11,9 +11,9 @@ export default [
         id:2,
         name: "Roronoa Zoro",
         img: "./images/Roronoa_Zoro_Portrait (1).jpg",
-        title: "Swordsman/First Mate",
+        title: "Swordsmen/First Mate",
         bounty: "1,111,000,000",
-        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, His dream is to become the greatest swordsman in the world, in order to honor a promise he made to his deceased childhood friend Kuina.", 
     },
     {
         id:3,
@@ -21,7 +21,7 @@ export default [
         img: "./images/Nami_Portrait.jpg",
         title: "Navigator",
         bounty: "366,000,000",
-        description: "\"Cat Burglar\" Nami is the navigator of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. She is the third member of the crew and the second to join, doing so during the Orange Town Arc. She is the adoptive sister of Nojiko after the two were orphaned and taken in by Bell-mère."
+        description: "\"Cat Burglar\" Nami is the navigator of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. She is the third member of the crew and the second to join, doing so during the Orange Town Arc. She is the adoptive sister of Nojiko after the two were orphaned and taken in by Bell-mère. Her dream is to make a map of the entire world."
     },
     {
         id:4,
@@ -29,7 +29,7 @@ export default [
         img: "./images/Usopp_Portrait.jpg",
         title: "Sniper",
         bounty: "500,000,000",
-        description: "\"God\" Usopp is the sniper of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the fourth member of the crew and the third to join, doing so at the end of the Syrup Village Arc. Although he left the crew during the Water 7 Arc, he rejoined at the end of the Post-Enies Lobby Arc."
+        description: "\"God\" Usopp is the sniper of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the fourth member of the crew and the third to join, doing so at the end of the Syrup Village Arc. Usopp dreams of becoming a brave warrior of the sea just like his father and lives every day in pursuit of living up to this dream."
     },
     {
         id:5,
@@ -37,46 +37,46 @@ export default [
         img:"./images/Sanji_Portrait.jpg",
         title: "Cook",
         bounty: "1,032,000,000",
-        description: "\"Black Leg\" Sanji, born as Vinsmoke Sanji, is the cook of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the fifth member of the crew and the fourth to join, doing so at the end of the Baratie Arc."
+        description: "\"Black Leg\" Sanji, born as Vinsmoke Sanji, is the cook of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the fifth member of the crew and the fourth to join, doing so at the end of the Baratie Arc. His dream is to find the rumored chef's paradise, All Blue, which is where East Blue, West Blue, North Blue, and South Blue meet, along with their wildlife. "
     },
     {
-        id:2,
-        name: "Roronoa Zoro",
-        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
-        title: "Swordsman/First Mate",
-        bounty: "1,111,000,000",
-        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+        id:6,
+        name: "Tony Tony Chopper",
+        img: "./images/Tony_Tony_Chopper_Portrait.jpg",
+        title: "Doctor",
+        bounty: "1,000",
+        description: "Tony Tony Chopper, also known as \"Cotton Candy Lover\" Chopper, is the doctor of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. His dream is to one day become a doctor capable of curing any disease and wants to travel all across the world specifically in the hopes of accomplishing this dream."
     },
     {
-        id:2,
-        name: "Roronoa Zoro",
-        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
-        title: "Swordsman/First Mate",
-        bounty: "1,111,000,000",
-        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+        id:7,
+        name: "Nico Robin",
+        img: "./images/Nico_Robin_Post_Timeskip_Portrait.jpg",
+        title: "Archaeologist",
+        bounty: "930,000,000",
+        description: "Nico Robin, also known by her epithet \"Devil Child\" and the \"Light of the Revolution\", is the archaeologist of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. She is the seventh member of the crew and the sixth to join. Her dream is to find the Rio Poneglyph which tells the true history of the world, specifically the Void Century."
     },
     {
-        id:2,
-        name: "Roronoa Zoro",
-        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
-        title: "Swordsman/First Mate",
-        bounty: "1,111,000,000",
-        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+        id:8,
+        name: "Franky",
+        img: "./images/Franky_Portrait.jpg",
+        title: "Shipwright",
+        bounty: "394,000,000",
+        description: "\"Iron Man\" Franky is the shipwright of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the crew's eighth member and the seventh to join. Franky's dream is to create a ship and circumnavigate the world with it, and he built the Thousand Sunny and joined the Straw Hat Pirates to fulfill his dream."
     },
     {
-        id:2,
-        name: "Roronoa Zoro",
-        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
-        title: "Swordsman/First Mate",
-        bounty: "1,111,000,000",
-        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+        id:9,
+        name: "Brook",
+        img: "./images/Brook_Portrait.jpg",
+        title: "Swordsmen/Musician",
+        bounty: "383,000,000",
+        description: "\"Soul King\" Brook is the musician of the Straw Hat Pirates, one of their two swordsmen and one of the Senior Officers of the Straw Hat Grand Fleet. He is the ninth member of the crew and the eighth to join. His dream is to reunite with his old friend, Laboon, at Reverse Mountain, where he resides with Crocus."
     },
     {
-        id:2,
-        name: "Roronoa Zoro",
-        img: "./images/Roronoa_Zoro_Portrait (1).jpg",
-        title: "Swordsman/First Mate",
-        bounty: "1,111,000,000",
-        description: "Roronoa Zoro, also known as \"Pirate Hunter\" Zoro, is a main combatant of the Straw Hat Pirates, one of their two swordsmen, one of the Senior Officers of the Straw Hat Grand Fleet, and is publicly recognized as the right-hand man of his crew's captain Monkey D. Luffy. Formerly a bounty hunter, he is the second member of Luffy's crew and the first to join it"
+        id:10,
+        name: "Jimbei",
+        img: "./images/Jinbe_Portrait.jpg",
+        title: "Helmsman",
+        bounty: "1,100,000,000",
+        description: "\"Knight of the Sea\" Jinbe is the helmsman of the Straw Hat Pirates and one of the Senior Officers of the Straw Hat Grand Fleet. He is the tenth member of the crew and the ninth to join. His dream is to fulfill his former captain Fisher Tiger's dying wish of coexistence and equality between humans and fish-men."
     }
 ]
